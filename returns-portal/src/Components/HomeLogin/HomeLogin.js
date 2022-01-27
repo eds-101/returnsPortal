1c53a5fc4507bee784f73ba3a2f269b801eb2ba0
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-01-27 11:57:54 +0000

    check user submission

--- returns-portal/src/Components/HomeLogin/HomeLogin.js
+++ returns-portal/src/Components/HomeLogin/HomeLogin.js
@@ -2,11 +2,30 @@ import './HomeLogin.css'
 
 function HomeLogin(){ 
 
-    function handleSubmit(e){
+    async function handleSubmit(e){
+        // Needs CORS approval when running on localhost
+
         e.preventDefault()
-        console.log(e.target[0].value)
-        console.log(e.target[1].value)
-        console.log(e.target[2].value)
+        const orderNumber = (e.target[0].value)
+        const emailAddress = (e.target[1].value)
+        const postcode = (e.target[2].value)
+
+        //api call
+        try {
+            const getOrderDetails = await fetch(`https://api.mintsoft.co.uk/api/Order/Search?APIKey=${process.env.API_KEY}&OrderNumber=PCP17043`)
+            const orderDetails = await getOrderDetails.json()
+            const userEmail = orderDetails[0].Email
+            const userPostCode = orderDetails[0].PostCode
+
+        } catch(error) {
+            console.error(e)
+        }
+
+    }
+
+    function authenticateUserDetails(){
+        // check api calls email/postcode === user submission
+
     }
 
     return(
