5e89b8a8f49863f346749dffcf54cf56efe57cea
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-02-09 13:23:51 +0000

    returns complete styling

--- returns-portal/src/Components/ReturnsComplete/ReturnsComplete.js
+++ returns-portal/src/Components/ReturnsComplete/ReturnsComplete.js
@@ -9,12 +9,20 @@ function ReturnsComplete(props){
 
     return(
         <div>
-            <div>
+            <div className='headline'>
+                <h2>
+                    Your return has been completed
+                </h2>
+                <h4>
+                    Confirmation of returned products:
+                </h4>
+            </div>
+            <div className='body'>
                 <ul>
                     {props.finalOrder.map((product) => {
                        return <li>{` ${product['Quantity'] > 1 ? product['Quantity'] + 'x' : ""}`} {product['Name'].slice(0,1) + product['Name'].slice(1).toLowerCase()}</li>
                     })}
-                    <button onClick={returnToHomePage}>Back to returns page</button>
+                    <button className='button' onClick={returnToHomePage}>Back to Returns page</button>
                 </ul>
             </div>
         </div>
